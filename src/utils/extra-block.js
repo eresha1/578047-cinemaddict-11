6dcfbabc6151e2b8519039b601de83878a63c8b6
Irina <eresha39@gmail.com>
2020-04-28 20:06:50 +0700

    module4-task2

--- src/utils/extra-block.js
+++ src/utils/extra-block.js
@@ -1,11 +1,15 @@
-export const getTopRatedMovies = (films) => {
-  return films.slice().sort(function (a, b) {
-    return b.rating - a.rating;
-  });
-};
+// export const getTopRatedMovies = (films) => {
+//   return films.slice().sort(function (a, b) {
+//     return b.rating - a.rating;
+//   });
+// };
 
-export const getMostCommentedMovies = (films) => {
-  return films.slice().sort(function (a, b) {
-    return b.commentsCount - a.commentsCount;
-  });
-};
+// export const getMostCommentedMovies = (films) => {
+//   return films.slice().sort(function (a, b) {
+//     return b.commentsCount - a.commentsCount;
+//   });
+// };
+
+export const getTopRatedMovies = (films) => films.slice().sort((a, b) => a.rating > b.rating ? -1 : 1);
+
+export const getMostCommentedMovies = (films) => films.slice().sort((a, b) => a.commentsCount > b.commentsCount ? -1 : 1);
