2f7f8c4dbc26d5cbdfab972ed64c29d8cbba2cc6
Irina <eresha39@gmail.com>
2020-05-30 20:46:54 +0700

    исправляет comments component

--- src/components/comments.js
+++ src/components/comments.js
@@ -20,18 +20,18 @@ const createEmojiListMarkup = (names) => {
 };
 const createCommentsMarkup = (comments) => {
   return comments.map((comment) => {
-    const {text, author, emoji, date} = comment;
+    const {text, author, emoji, date, id} = comment;
     return (
       `<li class="film-details__comment">
               <span class="film-details__comment-emoji">
-                <img src="images/emoji/${emoji}.png" width="55" height="55" alt="emoji-{emotion}">
+                <img src="images/emoji/${emoji}.png" width="55" height="55" alt="emoji-${emoji}">
               </span>
               <div>
                 <p class="film-details__comment-text">${text}</p>
                 <p class="film-details__comment-info">
                   <span class="film-details__comment-author">${author}</span>
                   <span class="film-details__comment-day">${date}</span>
-                  <button class="film-details__comment-delete">Delete</button>
+                  <button class="film-details__comment-delete" id="comment-${id}">Delete</button>
                 </p>
               </div>
             </li>`
@@ -39,12 +39,15 @@ const createCommentsMarkup = (comments) => {
   }).join(`\n`);
 };
 
-const creatCommentsTemplate = (bigCard, options = {}) => {
-  const {comments, commentsCount} = bigCard;
+const creatCommentsTemplate = (card, options = {}) => {
+  const {comments} = card;
   const {newComment} = options;
+
+  const currentEmoji = `${newComment.emoji}` ? `<img src="images/emoji/${newComment.emoji}.png" width="55" height="55" alt="emoji-${newComment.emoji}">` : ``;
+
   return (
     `<section class="film-details__comments-wrap">
-      <h3 class="film-details__comments-title">${comments.length === 1 ? `Comment` : `Comments`} <span class="film-details__comments-count">${commentsCount}</span></h3>
+      <h3 class="film-details__comments-title">${comments.length === 1 ? `Comment` : `Comments`} <span class="film-details__comments-count">${comments.length}</span></h3>
 
       <ul class="film-details__comments-list">
       ${createCommentsMarkup(comments)}
@@ -52,11 +55,11 @@ const creatCommentsTemplate = (bigCard, options = {}) => {
 
       <div class="film-details__new-comment">
         <div for="add-emoji" class="film-details__add-emoji-label">
-        ${newComment.emoji ? `<img src="images/emoji/${newComment.emoji}.png" width="55" height="55" alt="emoji-smile">` : ``}
+        ${currentEmoji}
       </div>
 
       <label class="film-details__comment-label">
-        <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment"></textarea>
+        <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment">${newComment.text}</textarea>
       </label>
 
       <div class="film-details__emoji-list">
@@ -68,34 +71,37 @@ const creatCommentsTemplate = (bigCard, options = {}) => {
 };
 
 export default class Comments extends AbstractSmartComponent {
-  constructor(bigCard) {
+  constructor(card) {
     super();
-    this._bigCard = bigCard;
-
+    this._comments = card.comments;
+    this._card = card;
+    console.log(this._comments)
     this._newComment = {
-      // text: ``,
+      text: ``,
       emoji: ``,
       // author: ``,
       // date: null,
     };
 
     this._subscribeOnEvents();
+    // this._deleteCommentsButtonClickHandler = null;
 
   }
 
   getTemplate() {
-    return creatCommentsTemplate(this._bigCard, {
+    return creatCommentsTemplate(this._card, {
       newComment: this._newComment
     });
   }
 
   recoveryListeners() {
     this._subscribeOnEvents();
+    this.setDeleteButtonClickHandler(this._deleteButtonClickHandler);
   }
 
   reset() {
     this._newComment = {
-      // text: ``,
+      text: ``,
       emoji: ``,
       // author: ``,
       // date: null,
@@ -108,12 +114,27 @@ export default class Comments extends AbstractSmartComponent {
     super.rerender();
   }
 
+    
+  setDeleteButtonClickHandler(handler) {
+    const deleteButtons = this.getElement().querySelectorAll(`.film-details__comment-delete`);
+    deleteButtons.forEach((deleteButton) => deleteButton.addEventListener(`click`, handler));
+    this._deleteButtonClickHandler = handler;
+  }
+
   _subscribeOnEvents() {
     const element = this.getElement();
     element.querySelector(`.film-details__emoji-list`)
       .addEventListener(`change`, (evt) => {
+        if (evt.target.tagName !== `INPUT`) {
+          return;
+        }
+
         this._newComment.emoji = evt.target.value;
         this.rerender();
       });
+
+    element.querySelector(`.film-details__comment-input`).addEventListener(`input`, (evt) => {
+      this._newComment.text = evt.target.value;
+    });
   }
 }
